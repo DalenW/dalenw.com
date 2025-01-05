83b7e48cc0aca227b76b4fa312789c7f95a57ca5
Dalen Ward <dalen.ward@m6securities.com>
2025-01-04 18:17:25 -0700

    improved index, show, edit

--- app/components/form/stackedit_component.rb
+++ app/components/form/stackedit_component.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class Form::StackeditComponent < ViewComponent::Base
+
+  def initialize(name, markdownContent)
+    @name = name
+    @content = markdownContent
+  end
 end
