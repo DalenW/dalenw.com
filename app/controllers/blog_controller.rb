b90f343d485fa0c63e8e43f9ab10a8076bde717a
DalenW <dalen.ward@greenixpc.com>
2025-01-08 10:06:29 -0700

    better post rendering

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -4,7 +4,6 @@ class BlogController < ApplicationController
   def index
     @posts = Post.all
     @post = @posts.last
-    @markdown = Redcarpet::Markdown.new(FluffyCarpet, fenced_code_blocks: true, tables: true)
 
     render "blog/post"
   end
@@ -16,8 +15,5 @@ class BlogController < ApplicationController
     if @post.nil?
       return render nothing: true, status: :not_found
     end
-
-    # @turbo_frame_tag = "post_#{@post.id}"
-    @markdown = Redcarpet::Markdown.new(FluffyCarpet, fenced_code_blocks: true, tables: true)
   end
 end
