ce7726f596203ba423710939b6add6e1751a5022
Dalen Ward <dalen.ward@m6securities.com>
2025-01-13 21:51:45 -0700

    hover links in posts

--- lib/fluffy_carpet.rb
+++ lib/fluffy_carpet.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
+# https://github.com/vmg/redcarpet#block-level-calls
 class FluffyCarpet < Redcarpet::Render::HTML
   include Redcarpet::Render::SmartyPants
+
+  def link(link, title, content)
+    %(<a class="link link-hover" href="#{link}">#{content}</a>)
+  end
 
   def paragraph(text)
     %(<p>#{text}</p>)
