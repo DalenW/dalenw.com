7895cb9f8d0fc6016c2ad6907d3ae9a2e7933e68
Dalen Ward <dalen.ward@m6securities.com>
2025-01-14 18:50:35 -0700

    working image carousel

--- lib/fluffy_carpet.rb
+++ lib/fluffy_carpet.rb
@@ -4,6 +4,10 @@
 class FluffyCarpet < Redcarpet::Render::HTML
   include Redcarpet::Render::SmartyPants
 
+  def image(link, title, alt_text)
+    %(<img src="#{link}" alt="#{alt_text}" loading="lazy" data-title="#{title}">)
+  end
+
   def link(link, title, content)
     %(<a class="link link-hover" href="#{link}">#{content}</a>)
   end
@@ -16,6 +20,33 @@ class FluffyCarpet < Redcarpet::Render::HTML
     case language
     when "mermaid"
        %(<div><pre class="mermaid">#{code}</pre></div>)
+    when "image_gallery"
+      html = %(<div class="carousel carousel-center w-full">)
+
+      parent_id = SecureRandom.alphanumeric(5)
+
+      lines = code.split("\n")
+      lines_length = lines.length
+
+      lines.each_with_index do |line, index|
+        item_id = "#{parent_id}_#{index}"
+
+
+        html += %(<div id="#{item_id}" class="carousel-item w-full">
+                    <img
+                      src="#{line}"
+                      class="object-contain" loading="lazy">
+                  </div>)
+      end
+
+      html += %(</div><div class="flex w-full justify-center gap-2 py-2">)
+
+      lines_length.times do |index|
+        item_id = "#{parent_id}_#{index}"
+        html += %(<a href="##{item_id}" class="btn btn-xs link link-hover" data-turbo="false">#{index + 1}</a>)
+      end
+
+      html + "</div>\n"
     else
       %(<div data-controller="markdown--code" data-markdown--code-language-value="#{language}">#{code}</div>)
     end
